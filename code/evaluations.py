141a3d67d1ec3c6df4c621155d1aeeb7c9e82702
VictorRubio98 <victor.rubiojornet98@gmail.com>
2024-06-24 04:36:01 +0200

    evaluations.py removed not used functions

--- code/evaluations.py
+++ code/evaluations.py
@@ -3,8 +3,6 @@ import argparse
 
 import torch
 
-import matplotlib.pyplot as plt
-
 
 def calculateAdv(dataset:torch.Tensor)->float:
     positive_label = dataset[dataset[:, -1] == 1][:, -2]
@@ -14,60 +12,7 @@ def calculateAdv(dataset:torch.Tensor)->float:
     # print(f'Probability of False Positives: {prob_FP}')
     # print(f'Probability of True Positives: {prob_TP}')
     return (prob_TP - prob_FP)
-
-def calculateAffected(dataset:torch.Tensor)->float:
-    positive_label = dataset[dataset[:, -1] == 1][:, -2]
-    negative_label = dataset[dataset[:, -1] == 0][:, -2]
-    p_affected = (positive_label == 1).float().sum().item()/len(positive_label)
-    return p_affected
     
-
-def calculateF1(dataset:torch.Tensor)->torch.Tensor:
-    positive_label = dataset[dataset[:, -1] == 1][:, -2]
-    negative_label = dataset[dataset[:, -1] == 0][:, -2]
-    TP = (positive_label == 1).float().sum().item()
-    TN = (negative_label == 0).float().sum().item()
-    FN = (positive_label == 0).float().sum().item()
-    FP = (negative_label == 1).float().sum().item()
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
-    print(f'Precision: {precision}')
-    print(f'Recall: {recall}')
-    f1 = 2*precision*recall/(precision+recall)
-    return (f1)
-    
-
-def calculatePG(baseline, e_private, threshold):
-    # Procedure for baseline
-    #baseline = torch.load(baseline_results)
-    #e_private = torch.load(e_private_results) no need since we work already with tensors.
-    
-    baseline_p_label = baseline[baseline[:, 49] == 1][:, 48]
-    baseline_n_label = baseline[baseline[:, 49] == 0][:, 48]
-    decision_threshold = (baseline_p_label.mean() + baseline_n_label.mean()) / 2
-    
-    # Procedure for e-private
-    e_private_p_label = e_private[e_private[:, 49] == 1][:, 48]
-    e_private_n_label = e_private[e_private[:, 49] == 0][:, 48]
-    decision_threshold_e = (e_private_p_label.mean() + e_private_n_label.mean()) / 2
-    
-    # Calculate probabilities for baseline
-    probTP_b = (baseline_p_label > decision_threshold).float().mean().item()
-    probFP_b = (baseline_n_label > decision_threshold).float().mean().item()
-    
-    # Calculate probabilities for e-private
-    probTP_e = (e_private_p_label > decision_threshold).float().mean().item()
-    probFP_e = (e_private_n_label > decision_threshold_e).float().mean().item()
-    
-    # Calculate privacy loss
-    privacy_gain = (probTP_b - probFP_b) - (probTP_e - probFP_e)
-    print(f"Baseline Prob TP: {probTP_b}")
-    print(f"Baseline Prob FP: {probFP_b}")
-    print(f"E-Private Prob TP: {probTP_e}")
-    print(f"E-Private Prob FP: {probFP_e}")
-    print(f"Privacy Gain: {privacy_gain}")
-    return torch.tensor(privacy_gain,dtype=float)
-
 if __name__ =='__main__':
     
     parser = argparse.ArgumentParser()
